40a418288ef720ac3b9669e8bf5c91fbfe50e717
Sandro <assandroa95@gmail.com>
2020-07-28 12:52:18 +0700

    image path fix

--- src/controllers/photo.js
+++ src/controllers/photo.js
@@ -3,7 +3,7 @@ const Photo = require('../models/photo');
 async function post(req, res, next) {
 	console.log(req.file);
 	console.log(req.body);
-	const photo = new Photo({ ...req.body, imagePath: req.file.filename });
+	const photo = new Photo({ ...req.body, imagePath: `uploads/${req.file.filename}` });
 	try {
         await photo.save();
 		res.status(201).send(photo);
